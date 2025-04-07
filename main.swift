22776bd8a9cca28e6e91228bc27c33cfc4319219
Osman <osmandall@outlook.com>
2025-04-08 02:40:23 +0300

    calculator.swift

--- main.swift
+++ main.swift
@@ -1,14 +1,14 @@
-import Foundation 
+import Foundation
 
 struct Calculator {
-  func add(_ a: Int, _b: Int) -> {
-    return a + b
-  } 
+    func add(_ a: Int, _ b: Int) -> Int {
+        return a + b
+    }
 
-  func subtract(_ a: Int, _ b: Int) -> {
-    return a - b
-  }
+    func subtract(_ a: Int, _ b: Int) -> Int {
+        return a - b
+    }
 }
 
 let calculator = Calculator()
-print("Toplam: \{calculator.add(5, 3))")
+print("Toplam: \(calculator.add(5, 3))")
